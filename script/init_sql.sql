178f2e72772ca33967c95bfbee261df6e6c344d8
1119209427 <1119209427@qq.com>
2023-12-19 23:52:45 +0800

    修改了consulConfig读取配置文件tag类型错误

--- script/init_sql.sql
+++ script/init_sql.sql
@@ -15,8 +15,6 @@ CERATE TABLE `t_comment`
     `video_id` bigint(20) NOT NULL COMMENT '评论视频id',
     `parent_id` bigint(20)  COMMENT '父评论的id',
     `parent_user_id` bigint(20)  COMMENT '父评论的user_id',
-    `reply_id`  bigint(20)  COMMENT '回复评论的id,默认为二级评论',
-    `reply_user_id` bigint(20)  COMMENT '回复评论的user_id,默认为二级评论',
     `comment_level` tinyint(4) NOT NULL COMMENT '评论的等级，1为一级评论，2为二级评论',
     `comment_text` varchar(255) NOT NULL COMMENT '评论内容',
     `create_time` datatime  NOT NULL COMMENT '评论发布时间',
@@ -24,3 +22,11 @@ CERATE TABLE `t_comment`
      KEY `idx_video` (`video_id`,`comment_level`) USING BTREE,
      KEY `idx_create_time` (`created_time`) USING BTREE,
 )
+
+DROP TABLE IF EXISTS `t_comment_replies`;
+CERATE TABLE `t_comment_replies`
+{
+    `id`  bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+    `comment_id` bigint(20) NOT NULL COMMENT '评论的id 与comment表中的id关联'
+    `reply_id` bigint(20) NOT NULL COMMENT '回复评论的id 与comment表中的id关联'
+}
